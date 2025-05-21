ba448bf787541c3366d8d9f72fc7388de2ee6973
sewen48 <gadelaja48@gmail.com>
2025-05-21 05:47:07 +0100

    Update jenkins.tf

--- JENKINS/jenkins.tf
+++ JENKINS/jenkins.tf
@@ -1,7 +1,7 @@
 # configured aws provider with proper credentials
 provider "aws" {
   region    = "us-east-1"
-  profile   = "yusuf"
+  profile   = "ganiyy"
 }
 
 
@@ -89,7 +89,7 @@ resource "aws_instance" "ec2_instance" {
   instance_type          = "t2.small"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group_jenkins.id]
-  key_name               = "devopskeypair"
+  key_name               = "zeeKP"
   # user_data            = file("install_jenkins.sh")
 
   tags = {
@@ -105,7 +105,7 @@ resource "null_resource" "name" {
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = file("~/Downloads/devopskeypair.pem")
+    private_key = file("~/Downloads/zeeKP.pem")
     host        = aws_instance.ec2_instance.public_ip
   }
 

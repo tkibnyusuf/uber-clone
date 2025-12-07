fd4ad8422caf18a849ba5b23caac76bc527e58c4
tkibnyusuf <85669533+tkibnyusuf@users.noreply.github.com>
2025-12-07 11:45:12 -0600

    Update eks-cluster.tf

--- EKS_Terraform/eks-cluster.tf
+++ EKS_Terraform/eks-cluster.tf
@@ -32,13 +32,13 @@ resource "aws_eks_cluster" "example" {
   name     = "EKS_CLOUD"
   role_arn = aws_iam_role.example.arn
   # Enable control plane logging
-  enabled_cluster_log_types = [
-    "api",
-    "audit",
-    "authenticator",
-    "controllerManager",
-    "scheduler"
-  ]
+ # enabled_cluster_log_types = [
+ #   "api",
+ #   "audit",
+ #   "authenticator",
+ #   "controllerManager",
+ #   "scheduler"
+ # ]
   vpc_config {
         subnet_ids = var.subnet_ids
   }
